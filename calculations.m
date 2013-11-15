1ffeddacb9d321344c4307b409e9a69fbe0ddea1
Ned Gulley <gulley@gmail.com>
2013-11-15 18:07:23 -0500

    Geometry almost complete

--- calculations.m
+++ calculations.m
@@ -3,32 +3,34 @@
 % Desired outputs is the projections onto the earth-sun-moon plane
 
 %%
-% Positions of [sun mercury venus earth moon mars]
+% Positions of [sun mercury venus earth moon mars jupiter saturn]
 
 p = [ ...
-    98737.65714089875 -322333.36244940694 -153695.74901326824
-    6941996.601020642 40110318.72735843 20735265.058509897
-    107376698.98967445 16606844.809812635 674753.2904573642
-    102641024.09369242 97848513.33584224 42404420.99743636
-    102797481.67090186 97531216.1543806 42302953.91258208
-    -164532329.9741314 165301295.37539384 80258023.65923128];
-nm = {'sun','mercury','venus','earth','moon','mars'};
+    105466.57862061243 -320513.0525348927 -153075.22353957177
+    -28254617.546911404 33413899.08547402 20807485.255014483
+    101892433.53560476 35869153.31981312 9688685.825101694
+    87994759.91391698 108869760.5619592 47182570.1411031
+    88318269.65765092 109070078.26608586 47271779.78689272
+    -175033175.6172045 156838581.5712689 76659903.66643791
+    -147654424.0403505 696873333.1511024 302281137.98811287
+    -1054977146.3189284 -970294923.8394519 -355362682.91360706];
+nm = {'Sun','Mercury','Venus','Earth','Moon','Mars','Jupiter','Saturn'};
 
 planet = [];
 % First assign the positions
 for i = 1:size(p,1)
-    pi = p(i,:);
-    planet(i).p = pi;
-    planet(i).nm = nm{i}
-
-    plot3(pi(1),pi(2),pi(3),'r.');
-    text(pi(1),pi(2),pi(3),[' ' planet(i).nm]);
+    p_i = p(i,:);
+    planet(i).p = p_i;
+    planet(i).nm = nm{i};
+    
+    plot3(p_i(1),p_i(2),p_i(3),'r.');
+    text(p_i(1),p_i(2),p_i(3),[' ' planet(i).nm]);
     hold on
 end
 hold off
 
 
-%% 
+%%
 % Now calculate the geocentric position vectors of each planet
 
 pearth = planet(4).p;
@@ -37,7 +39,7 @@ for i = 1:length(planet)
     % pen = normalized position relative to earth
     % (i.e. a unit vector pointing from earth to body X
     pen = pe/sqrt(dot(pe,pe));
-    planet(i).pen = pen;       
+    planet(i).pen = pen;
     
     plot3([0 pen(1)],[0 pen(2)],[0 pen(3)],'r.-');
     text(pen(1),pen(2),pen(3),[' ' planet(i).nm]);
@@ -69,11 +71,39 @@ end
 hold off
 
 %%
-% cos(theta) = dot(u,v)/(sqrt(dot(u,u))*sqrt(sqrt(dot(v,v))))
+% Calculate the angle between the sun and each of the bodies
 
-x = cross(planet(1).pen2,planet(5).pen2)
-y = cross(planet(1).pen2,planet(3).pen2)
-x./y
+sun = planet(1).pen2;
+planet(1).theta = 0;
 
-% http://stackoverflow.com/questions/15101103/euler-angles-between-two-3d-vectors
+for i = [2 3 5 6 7 8]
+    body = planet(i).pen2;
+    cosTheta = dot(sun,body)/(sqrt(dot(sun,sun))*sqrt(dot(body,body)));
+    theta = acos(cosTheta);
+    % The sun-moon cross vector sticks out of the plane. We can use it to
+    % determine the orientation of theta
+    
+    x = cross(sun,body);
+    theta = theta*sign(dot(b,x));
+    planet(i).theta = theta;
+    
+end
+
+%% Plot the result
+
+cla
+for i = [1 2 3 5 6 7 8]
+    beta = planet(i).theta + pi/2;
+    x = cos(beta);
+    y = sin(beta);
+    line([0 x],[0 y])
+    line(x,y,'Marker','o','MarkerSize',6)
+    text(x,y,['  ' planet(i).nm]) 
+end
+line(0,0,'Marker','o','MarkerSize',9)
+
+axis equal
+axis(2*[-1 1 -1 1])
+% axis square
+box on
 
--- skyclock.js
+++ skyclock.js
@@ -6,44 +6,35 @@ SC.SkyClock = {
   
   startUp : function() {
   	// See http://davywybiral.blogspot.com/2011/11/planetary-states-api.html
-  	var url = 'http://www.astro-phys.com/api/states?callback=?';
-	$.getJSON(url, {bodies: 'sun,mercury,venus,earth,moon,mars'}, function(data) {
-		SC.SkyClock.showSky(data.results);
-	});
+    var url = 'http://www.astro-phys.com/api/states?callback=?';
+    $.getJSON(url, {bodies: 'sun,mercury,venus,earth,moon,mars,jupiter,saturn'}, function(data) {
+	 	  SC.SkyClock.showSky(data.results);
+	  });
   },
 
   showSky : function(results) {
     var td = new Date();
 
-	var pMercury = results.mercury[0];
-	var pVenus   = results.venus[0];
-	var pSun     = results.sun[0];
-	var pEarth   = results.earth[0];
-	var pMoon    = results.moon[0];
-	var pMars    = results.mars[0];
+  	var pMercury = results.mercury[0];
+  	var pVenus   = results.venus[0];
+  	var pSun     = results.sun[0];
+  	var pEarth   = results.earth[0];
+  	var pMoon    = results.moon[0];
+    var pMars    = results.mars[0];
+    var pJupiter = results.jupiter[0];
+    var pSaturn  = results.saturn[0];
 
     $(".time").html(
       "Today is " + td.toDateString() + "<br/>"
-      + "The Sun can be found at " + pSun[0] + " " + pSun[1] + " " + pSun[2] + "<br/>"
-      + "Mercury can be found at " + pMercury[0] + " " + pMercury[1] + " " + pMercury[2] + "<br/>"
-      + "Venus can be found at " + pVenus[0] + " " + pVenus[1] + " " + pVenus[2] + "<br/>"
-      + "Earth can be found at " + pEarth[0] + " " + pEarth[1] + " " + pEarth[2] + "<br/>"
-      + "Moon can be found at " + pMoon[0] + " " + pMoon[1] + " " + pMoon[2] + "<br/>"
-      + "Mars can be found at " + pMars[0] + " " + pMars[1] + " " + pMars[2] + "<br/>"
+      + "Sun: " + pSun[0] + " " + pSun[1] + " " + pSun[2] + "<br/>"
+      + "Mercury: " + pMercury[0] + " " + pMercury[1] + " " + pMercury[2] + "<br/>"
+      + "Venus: " + pVenus[0] + " " + pVenus[1] + " " + pVenus[2] + "<br/>"
+      + "Earth: " + pEarth[0] + " " + pEarth[1] + " " + pEarth[2] + "<br/>"
+      + "Moon: " + pMoon[0] + " " + pMoon[1] + " " + pMoon[2] + "<br/>"
+      + "Mars: " + pMars[0] + " " + pMars[1] + " " + pMars[2] + "<br/>"
+      + "Jupiter: " + pJupiter[0] + " " + pJupiter[1] + " " + pJupiter[2] + "<br/>"
+      + "Saturn: " + pSaturn[0] + " " + pSaturn[1] + " " + pSaturn[2] + "<br/>"
     );
 
-    // To get geocentric angles, subject earth position from sun and a planet at a time
-    // Use: cos(theta) = (px1*px2 + py1*py2 + pz1*pz2)/ ...
-    //                (sqrt(px1^2 + py1^2 + pz1^2) * sqrt(px2^2 + py2^2 + pz2^2))
-    // Ref: http://www.geom.uiuc.edu/docs/reference/CRC-formulas/node54.html
-
-    /*
-          98275   -322455  -153737
-		9437030  39803620 20312736
-		107593109  15250412    50745
-		103588052  97031536 42050274
-		 103701377  96699886 41941231
-		-163790305 165861119 80494766
-    */
   }
 }

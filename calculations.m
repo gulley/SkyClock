b13bfcee8a97389da31b8bc1fd93d61fbc2f7a3b
Ned Gulley <gulley@gmail.com>
2014-04-07 16:41:04 -0400

    Improving calculations

--- calculations.m
+++ calculations.m
@@ -1,154 +1,229 @@
-% Sky Clock calculations
-% As an input we get the positions of the planets
-% Desired outputs is the projections onto the earth-sun-moon plane
-
-% http://davywybiral.blogspot.com/2011/11/planetary-states-api.html
-
-%%
-% Let's say we're interested in plotting the actual current state of the
-% solar system. To be specific, there are seven naked-eye heavenly bodies
-% in our neighborhood: sun, moon, mercury, venus, mars, saturn, Jupiter. I
-% want to plot them in their classic peas-on-a-plate configuration, as seen
-% from "above". Everything's relative in space, so when I say "above", I
-% mean looking down at the sun as the earth sweeps a counterclockwise
-% orbit.
+%% Sky Clock calculations
 %
-% See http://www.starchamber.com/skyclock/explanation.html for more notes.
+% I look up at the sky just after sunset and I see an especially bright
+% star. It's probably a planet. But which one? This sort of question gives
+% me a good opportunity to play around with MATLAB. Let's do a
+% visualization that shows where the planets are relative to the earth and
+% the sun.
 %
-% So the problem is this: first get the data from the web. Then mash all
-% the planets into a plane. Then figure out the angles to planets sun and
-% moon when looking from earth. It's a nice little 3-D geometry problem,
-% and gives us an opportunity to explore MATLAB.
+% <<../explanation1.png>>
+%
+% Here is the basic grade-school illustration of the solar system, the one
+% that shows the planets rolling around the Sun like peas on a plate. For
+% simplicity, we're just showing the sun, the earth, the moon, Venus, and
+% Mars. Viewed from far above the sun, it looks something like this.
+%
+% <<../explanation2.png>>
+%
+% But we don't really have the luxury of this high-level view. Instead, we
+% see little bright spots on a dark background somewhere "up there." Let's
+% reduce our problem to determining what direction the planets are in. This
+% leads to an image like this.
+%
+% <<../explanation3.png>>
+%
+% Our goal is to make an accurate up-to-date version of this diagram.
+% Specifically, in the plane of the ecliptic, and relative rto the sun,
+% where should we look to find the moon and the naked-eye planets (Mercury,
+% Venus, Mars, Jupiter, and Saturn)? To do this, we need to solve a few
+% problems.
+%
+% * Where are the planets?
+% * Find the vector pointing from earth to each planet
+% * Squash all these vectors into a single plane
+% * Visualize the resulting disk
 
-%% Planetary Positions
-% First of all, where are the planets? There's a free JSON service for
-% everything these days, or so it seems. One that I've found for planetary
-% data is hosted here http://davywybiral.blogspot.com/2011/11/planetary-states-api.html
-% Davy Wybiral
+%% Where Are the Planets?
+% First of all, where are the planets? There's a free JSON service for just
+% about everything these days. Here's one that I've found for
+% planetary data is hosted on Davy Wybiral's sitt here:
+%
+% <http://davywybiral.blogspot.com/2011/11/planetary-states-api.html>
 
 url = 'http://www.astro-phys.com/api/de406/states?bodies=sun,moon,mercury,venus,earth,mars,jupiter,saturn';
 json = urlread(url);
-data = JSON.parse(json);
+
+%% Parse the data
+% Now we can use
+% <http://www.mathworks.com/matlabcentral/fileexchange/42236-parse-json-text
+% Joe Hicklin's JSON parser> from the File Exchange. It returns a
+% well-bahaved MATLAB structure.
+
+data = JSON.parse(json)
+
+%%
+
+data.results
+
+%%
+% The distances are in kilometers, and I'm not even sure how this
+% representation is oriented relative to the surrounding galaxy. But it
+% doesn't really matter, because all I care about is the relative positions
+% of the bodies in question.
 
 %% Aerospace Toolbox Ephemeris
-% It turns out we can use the Aerospace Toolbox to get the same
-% information.
+% Note: We can also use the Aerospace Toolbox to get the same information.
+%
+% |[pos,vel] = planetEphemeris(juliandate(now),'Sun','Earth')|
 
-% [pos,vel] = planetEphemeris(juliandate(now),'Sun','Earth')
+%% Build the Solar System Structure
 
-% TODO: Does this square with what my other source is telling me?
+% List of bodies we care about
+ssList = {'sun','moon','mercury','venus','earth','mars','jupiter','saturn'};
 
-%%
-
-% List of bodies we care about 
-bList = {'sun','moon','mercury','venus','earth','mars','jupiter','saturn'};
-
-% 1 Astronomical Unit = 149 597 871 kilometers
-au = 149597871;
-
-% We'll store our distances in AU.
-b = [];
-for i = 1:length(bList)
-    bName = bList{i};
-    b(i).name = bName;
-    bVec = data.results.(bName);
-    b(i).position = bVec(1:3)/au;
-    b(i).velocity = bVec(4:6);
+ss = [];
+for i = 1:length(ssList)
+    ssObjectName = ssList{i};
+    ss(i).name = ssObjectName;
+    ssVec = data.results.(ssObjectName);
+    ss(i).position = ssVec(1:3);
 end
-
-%%
-% Make the sun stationary and at the center of the coordinate system.
-
-for i = 1:length(b)
-    b(i).position = b(i).position - b(1).position;
-    b(i).velocity = b(i).velocity - b(1).velocity;
-end
-
+ss
 
 %% Plot the planets
 
-for i = 1:length(b)-2
-    p = b(i).position;
+clf
+for i = 1:length(ss)
+    p = ss(i).position;
     plot3(p(1),p(2),p(3),'r.');
-    text(p(1),p(2),p(3),[' ' b(i).name]);
-    hold on
-end
-hold off
-
-%%
-% Now calculate the geocentric position vectors of each planet
-
-pearth = planet(4).p;
-for i = 1:length(planet)
-    pe = planet(i).p - pearth;
-    % pen = normalized position relative to earth
-    % (i.e. a unit vector pointing from earth to body X
-    pen = pe/sqrt(dot(pe,pe));
-    planet(i).pen = pen;
     
-    plot3([0 pen(1)],[0 pen(2)],[0 pen(3)],'r.-');
-    text(pen(1),pen(2),pen(3),[' ' planet(i).nm]);
+    text(p(1),p(2),p(3),[' ' ss(i).name]);
     hold on
 end
-% hold off
+
+hold off
+axis equal
 
 %%
-% psun is the unit vector that points from earth to sun
-% pmoon is the unit vector that points from earth to moon
+% This is accurate, but not yet very helpful. As we said earlier, from our
+% point of view here on the ground, the sun DOES go around the earth, as do
+% the moon and all the planets.
+%
+% Let's calculate the geocentric position vectors of each planet. To do
+% this, we'll put the earth at the center of the system. Copernicus won't
+% mind, because A) he's dead, and B) we admit this reference frame orbits
+% the sun. That's no worry to us here since we're just looking at
+% instantaneous positions in space.
+%
+% We're also going to use another file from the File Exchange.  Georg
+% Stillfried's <http://www.mathworks.com/matlabcentral/fileexchange/25372
+% mArrow3>
 
-psun = planet(1).pen;
-pmoon = planet(5).pen;
-b = cross(psun,pmoon);
+addpath([fxpath(25372) filesep])
+
+clf
+pEarth = ss(5).position;
+for i = 1:length(ss)
+    % pe = position relative to earth
+    % (i.e. a vector pointing from earth to body X)
+    pe = ss(i).position - pEarth;
+    % pne = normalized position relative to earth
+    pne = pe/sqrt(dot(pe,pe));
+    ss(i).pne = pne;
+    
+    mArrow3([0 0 0],pne, ...
+        'stemWidth',0.01,'FaceColor',[1 0 0]);
+    
+    text(pne(1),pne(2),pne(3),[' ' ss(i).name]);
+    hold on
+end
+light
+hold off
+axis equal
+axis off
 
 %%
+% These are unit vectors pointing out from the center of the earth towards
+% each of the other objects.
+
+
+%%
+% If we change our view point to look at the system edge-on, we can see the
+% objects are not quie co-planar. For simplicity, let's squash them all
+% into the same plane. We'll use the plane defined by the earth, moon, and
+% sun.
+
+% vsun is the unit vector that points from earth to sun
+% vmoon is the unit vector that points from earth to moon
+% The cross product of these two vectors will define the desired plane.
+
+% cla
+psun = ss(1).pne;
+pmoon = ss(2).pne;
+psunXpmoon = cross(psun,pmoon);
+mArrow3([0 0 0],psunXpmoon, ...
+    'stemWidth',0.01,'FaceColor',[0 0 0]);
+view(-45,15)
+a = axis
+axis(a/2)
+set(gca,'Clipping','off')
+
+%%
+% You can see that some of the planets deviate from this plane. The planets
+% stay close to the ecliptic, but they do wander a little.
 % Now we project the vectors onto the plane defined by earth-moon-sun
 % See http://en.wikipedia.org/wiki/Vector_projection
 
-for i = 1:length(planet)
-    pen = planet(i).pen;
-    pen2 = pen - dot(pen,b)/dot(b,b)*b;
-    planet(i).pen2 = pen2;
+hold on
+for i = 1:length(ss)
+    pne = ss(i).pne;
+    pneProj = pne - dot(pne,psunXpmoon)/dot(psunXpmoon,psunXpmoon)*psunXpmoon;
+    ss(i).pneProj = pneProj;
     
-    plot3([0 pen2(1)],[0 pen2(2)],[0 pen2(3)],'b.-');
-    text(pen2(1),pen2(2),pen2(3),[' ' planet(i).nm]);
-    hold on
+    mArrow3([0 0 0],pneProj, ...
+        'stemWidth',0.01,'FaceColor',[0 0 1]);
+    text(pneProj(1),pneProj(2),pneProj(3),[' ' ss(i).name]);
 end
 hold off
+axis equal
 
 %%
 % Calculate the angle between the sun and each of the bodies
+%
+% $$ cos(\theta) =   $$
 
-sun = planet(1).pen2;
-planet(1).theta = 0;
+sun = ss(1).pneProj;
+ss(1).theta = 0;
 
-for i = [2 3 5 6 7 8]
-    body = planet(i).pen2;
-    cosTheta = dot(sun,body)/(sqrt(dot(sun,sun))*sqrt(dot(body,body)));
+for i = 1:length(ss)
+    pneProj = ss(i).pneProj;
+    cosTheta = dot(sun,pneProj)/(sqrt(dot(sun,sun))*sqrt(dot(pneProj,pneProj)));
     theta = acos(cosTheta);
+    
     % The sun-moon cross vector sticks out of the plane. We can use it to
     % determine the orientation of theta
     
-    x = cross(sun,body);
-    theta = theta*sign(dot(b,x));
-    planet(i).theta = theta;
+    x = cross(sun,pneProj);
+    theta = theta*sign(dot(psunXpmoon,x));
+    ss(i).theta = theta;
     
 end
 
 %% Plot the result
 
 cla
-for i = [1 2 3 5 6 7 8]
-    beta = planet(i).theta + pi/2;
+
+k1 = 1.5;
+k2 = 1.2;
+for i = 1:length(ss)
+    beta = ss(i).theta + pi/2;
     x = cos(beta);
     y = sin(beta);
-    line([0 x],[0 y])
-    line(x,y,'Marker','o','MarkerSize',6)
-    text(x,y,['  ' planet(i).nm]) 
+    mArrow3([0 0 0],[x y 0], ...
+        'stemWidth',0.01, ...
+        'FaceColor',[0 0 1])
+    line([0 k1*x],[0 k1*y],'Color',0.8*[1 1 1])
+    text(k1*x,k1*y,ss(i).name,'HorizontalAlignment','center')
 end
-line(0,0,'Marker','o','MarkerSize',9)
+t = linspace(0,2*pi,100);
+line(k2*cos(t),k2*sin(t),'Color',0.8*[1 1 1])
+line(0,0,1,'Marker','.','MarkerSize',40,'Color',[0 0 1])
 
 axis equal
 axis(2*[-1 1 -1 1])
-% axis square
-box on
+
+%%
+% And there you have it: an accurate map of where the planets are in the
+% sky for today's date.
+
 
